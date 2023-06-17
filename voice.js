0b106c337ea2a8a231d670fdf62ad360a8ece4b5
mysynthesizer <64316449+mysynthesizer@users.noreply.github.com>
2023-06-17 12:13:54 +0300

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,9 +1,6 @@
 navigator.mediaDevices.getUserMedia({ audio: true})
     .then(stream => {
         const mediaRecorder = new MediaRecorder(stream)});
-navigator.mediaDevices.getUserMedia({ audio: true})
-    .then(stream => {
-        const mediaRecorder = new MediaRecorder(stream);
         let voice = [];
         document.querySelector('#start').addEventListener('click', function(){
             mediaRecorder.start();
