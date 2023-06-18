2d1d90a9ef802bc6f37b1391a86af1ad640d890a
mysynthesizer <64316449+mysynthesizer@users.noreply.github.com>
2023-06-18 17:10:19 +0300

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,23 +1,54 @@
+const URL = 'voice.php';
+let div = document.createElement('div');
+div.id = 'messages';
+let start = document.createElement('button');
+start.id = 'start';
+start.innerHTML = 'Start';
+let stop = document.createElement('button');
+stop.id = 'stop';
+stop.innerHTML = 'Stop';
+document.body.appendChild(div);
+document.body.appendChild(start);
+document.body.appendChild(stop);
 navigator.mediaDevices.getUserMedia({ audio: true})
     .then(stream => {
-        const mediaRecorder = new MediaRecorder(stream)});
-        let voice = [];
+        const mediaRecorder = new MediaRecorder(stream);
+
         document.querySelector('#start').addEventListener('click', function(){
-alert(3);
             mediaRecorder.start();
         });
-
+        let audioChunks = [];
         mediaRecorder.addEventListener("dataavailable",function(event) {
-            voice.push(event.data);
+            audioChunks.push(event.data);
         });
 
         document.querySelector('#stop').addEventListener('click', function(){
             mediaRecorder.stop();
         });
+
+        mediaRecorder.addEventListener("stop", function() {
+            const audioBlob = new Blob(audioChunks, {
+                type: 'audio/wav'
+            });
+
+            let fd = new FormData();
+            fd.append('voice', audioBlob);
+            sendVoice(fd);
+            audioChunks = [];
+        });
     });
-mediaRecorder.addEventListener("stop", function() {
-    const voiceBlob = new Blob([voice], {
-        type: 'audio/wav'
-    });
-    au.src=URL.createObjectURL(voiceBlob);
-});
+
+async function sendVoice(form) {
+    let promise = await fetch(URL, {
+        method: 'POST',
+        body: form});
+    if (promise.ok) {
+        let response =  await promise.json();
+        console.log(response.data);
+        let audio = document.createElement('audio');
+        audio.src = response.data;
+        audio.controls = true;
+        audio.autoplay = true;
+        document.querySelector('#messages').appendChild(audio);
+    }
+}
