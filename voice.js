61a107cf64b5e9d5a7d7d1c263e4da2b4fdeff9d
mysynthesizer <64316449+mysynthesizer@users.noreply.github.com>
2023-07-07 23:26:21 +0300

    Update voice.js

--- voice.js
+++ voice.js
@@ -4,7 +4,7 @@ let div = document.createElement('div');
 div.id = 'messages';
 let mystart = document.createElement('button');
 mystart.id = 'mystart';
-mystart.innerHTML = 'Start2';
+mystart.innerHTML = 'Start3';
 let stop = document.createElement('button');
 stop.id = 'stop';
 stop.innerHTML = 'Stop';
@@ -53,9 +53,10 @@ console.log(rrr);
             const audioBlob = new Blob(audioChunks, {
                 type: 'audio/wav'
             });
-au.src=mya.href=URL.createObjectURL(audioBlob);
- // au.play();
-            mya.download="rty";
+//au.src=mya.href=URL.createObjectURL(audioBlob);
+ au.src="track1.mp3";
+		// au.play();
+    //        mya.download="rty";
 
 var audioCtx = new AudioContext();
 	var source   = audioCtx.createMediaElementSource(au);
