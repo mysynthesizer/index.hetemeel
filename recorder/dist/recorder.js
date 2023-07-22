3bb43e8f1de02745231d26558f738565944e8262
mysynthesizer <64316449+mysynthesizer@users.noreply.github.com>
2023-07-22 08:19:50 +0300

    Update recorder.js

--- recorder/dist/recorder.js
+++ recorder/dist/recorder.js
@@ -1,5 +1,5 @@
 //btn.onclick=function(){
-qwert.innerText="rty5";
+qwert.innerText="rty7";
 //let vvv=1.5;
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Recorder = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
@@ -202,7 +202,7 @@ var Recorder = exports.Recorder = (function () {
            
    //             let vvv=1.2;
 
-let vvv=2**(Math.randim()*2-1);
+let vvv=2**(Math.random()*2-1);
                 console.log(vvv);
                 
                 var buffer = new ArrayBuffer(44 + samples.length * 2);
